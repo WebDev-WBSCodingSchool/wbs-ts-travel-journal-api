9d776ce3383a7e29aaca5fe2f943bd6a119bb7f9
Renke Brixel <renke.brixel@gmail.com>
2026-04-14 20:48:53 +0200

    Reapply "- Update TypeScript to v6"
    This reverts commit f8ae9c799c0d235073975406e9c08e1ef4a7cbdb.

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,7 +1,9 @@
-import { type ErrorRequestHandler } from 'express';
+import type { ErrorRequestHandler } from 'express';
 
 const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
-  process.env.NODE_ENV !== 'production' && console.log(`\x1b[31m${err.stack}\x1b[0m`);
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(`\x1b[31m${err.stack}\x1b[0m`);
+  }
 
   let errorMessage = 'Internal server error';
   let statusCode = 500;
